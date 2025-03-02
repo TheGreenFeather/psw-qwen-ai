56e4450862080a4874f67e0bf5ed88bdbdd4c1cb
Nguyen Vuong <nguyenvuong17102008@gmail.com>
2025-03-02 09:47:02 +0700

    Update CORS configuration in worker.js to allow specific origin alongside wildcard

--- worker.js
+++ worker.js
@@ -82,7 +82,7 @@ addEventListener("fetch", (event) => {
 const corsHeaders = {
   "Access-Control-Allow-Headers": "*", // What headers are allowed. * is wildcard. Instead of using '*', you can specify a list of specific headers that are allowed, such as: Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Authorization.
   "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS", // Allowed methods. Others could be GET, PUT, DELETE etc.
-  "Access-Control-Allow-Origin": "*", // This is URLs that are allowed to access the server. * is the wildcard character meaning any URL can.
+  "Access-Control-Allow-Origin": "http://localhost:5173, *", // This is URLs that are allowed to access the server. * is the wildcard character meaning any URL can.
 };
 
 async function handleRequest(request) {
